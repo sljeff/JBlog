f2ace6edc3db8884fbf1ded9a3a1cf75efb072f7
Jeff <shanliangjeff@qq.com>
2016-08-14 18:56:10 +0800

    add template, css, js

---
+++ static/js/main.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+  var leftDiv = document.getElementById('firstDiv');
+  var critileTop = leftDiv.offsetHeight - window.innerHeight;
+  if(window.innerWidth > 600){
+    if(critileTop <= 0){
+      leftDiv.style.height = window.innerHeight+'px';
+      leftDiv.style.position = 'fixed';
+    }else{
+      document.addEventListener('scroll', function(e){
+        if(window.scrollY >= critileTop){
+          leftDiv.style.top = -critileTop+'px';
+          leftDiv.style.position = 'fixed';
+        }else{
+          leftDiv.style.top = 0;
+          leftDiv.style.position = 'absolute';
+        }
+      });
+    }
+  }else{
+    window.scrollTo(0, leftDiv.clientHeight-50);
+  }
+}
