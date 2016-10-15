8ea2c5640ad67dc99c418d26f2455d73f2c2caaf
sljeff <shanliangjeff@qq.com>
2016-10-15 16:56:31 +0800

    write main.py

--- main.py
+++ main.py
@@ -1,43 +1,51 @@
 # coding: utf-8
 import os
-import tornado.ioloop
+import asyncio
+import tornado.platform.asyncio
 import tornado.web
-
-with open('test/content.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-
-class TestHandler(tornado.web.RequestHandler):
-    options = {
-        'mainTitle': '人的想法和意识',
-        'content': content,
-        'preLink': 'a',
-        'nextLink': 'b',
-        'pageTitle': '人的想法和意识——善良的杰夫',
-        'faviconLink': 'http://7xsn8i.com2.z0.glb.clouddn.com/img/favicon.ico',
-        'headPicLink': 'http://7xsn8i.com2.z0.glb.clouddn.com/img/head.jpg',
-        'blogName': '善良的杰夫',
-        'categories': [('/c/think', '随想'), ('/c/think', '编程'), ('/c/think', 'Node'), ('/c/think', '记录')],
-        'dates': [('/t/2016', '2016冬'), ('/t/2016', '2016秋'), ('/t/2016', '2016夏'), ('/t/2016', '2016春')] * 4,
-        'articleAuthor': 'Jeff',
-        'articleDate': '2016-05-27 01:08:39'
-    }
-
-    def get(self):
-        self.render("template/article.html", **self.options)
-
-
-class CSSHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write()
-
+from tornado.options import define, options
+import handlers
+import config
 
 setting = {
-    'static_path': os.path.join(os.path.dirname(__file__), "static")
+    'static_path': os.path.join(os.path.dirname(__file__), "static"),
+    'template_path': os.path.join(os.path.dirname(__file__), 'template')
 }
 
-app = tornado.web.Application([
-    (r'/', TestHandler),
+define('favicon_link', os.path.join(setting['static_path'], 'favicon.ico'))
+define('head_pic_link', os.path.join(setting['static_path'], 'head.jpg'))
+define('blog_name', config.blog_name)
+define('categories', config.categories)
+define('dates', config.dates)
+define('article_num', config.article_num)
+
+tornado.platform.asyncio.AsyncIOMainLoop().install()
+loop = asyncio.get_event_loop()
+
+
+class BlogApplication(tornado.web.Application):
+    def __init__(self, handlers=None, default_host="", transforms=None, **settings):
+        super(BlogApplication, self).__init__(handlers, default_host, transforms, **settings)
+        self.loop = loop
+        self.article_num = options.article_num
+        self.opts = {
+            'faviconLink': options.favicon_link,
+            'headPicLink': options.head_pic_link,
+            'blogName': options.blog_name,
+            'categories': options.categories,
+            'dates': options.dates
+        }
+        self.get_site_title = lambda title: str(title) + ' - ' + self.opts['blogName']
+
+
+app = BlogApplication([
+    (r'/', handlers.TimeHandler),
+    (r'/t/(.*)_to_(.*)', tornado.web.RedirectHandler, dict(url=r'/t/\1_to_\2/0')),
+    (r'/t/(.*)_to_(.*)/(.*)', handlers.TimeHandler),
+    (r'/c/(.*)', tornado.web.RedirectHandler, dict(url=r'/c/\1/0')),
+    (r'/c/(.*)/(.*)', handlers.CategoryHandler),
+    (r'/a/(.*)', handlers.ArticleHandler),
+    (r'/add', handlers.AddHandler),
 ], **setting)
-app.listen(8888)
-tornado.ioloop.IOLoop.current().start()
+app.listen(8765)
+loop.run_forever()
